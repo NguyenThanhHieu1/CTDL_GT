5371cf8ebc1ba19c60ea0029130ad91851386a76
NguyenThanhHieu1 <91010832+NguyenThanhHieu1@users.noreply.github.com>
2021-09-20 09:45:17 +0700

    ChuongTrinhLab2Bai4.java

---
+++ src/vn/edu/giadinh/tuan03/ChuongTrinhLab2Bai4.java
@@ -0,0 +1,38 @@
+package vn.edu.giadinh.tuan03;
+
+import java.util.Scanner;
+
+public class ChuongTrinhLab2Bai4 {
+    public static void main(String[] args){
+        System.out.println("========Menu=======");
+        System.out.println(">>1. Giai phuong trinh bac I");
+        System.out.println(">>2. Giai phuong trinh bac II");
+        System.out.println(">>3. Tinh tien dien");
+        System.out.println("========Menu=======");
+        int suLuaChon;
+        System.out.print("Lua chon chuc nang:");
+        Scanner bienNhap;
+        bienNhap = new Scanner(System.in);
+        suLuaChon = bienNhap.nextInt();
+
+        switch(suLuaChon){
+            case 1: giaiPTBacI(); break;
+            case 2: giaiPTBacII(); break;
+            case 3: tinhTienDien(); break;
+            default: System.out.println("Chon chuc nang khac");
+        }
+    }
+
+    static void giaiPTBacI(){
+         System.out.println("Trien khai code giai phuong trinh bac 1....");
+    }
+
+    static void giaiPTBacII(){
+        System.out.println("Trien khai code giai phuong trinh bac 2....");
+   }
+
+   static void tinhTienDien(){
+    System.out.println("Trien khai code tinh tien dien....");
+}
+}
+
